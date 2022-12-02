f955ed09a2b1025176f1b27b3a22659aa07e4294
Joseph2718 <67449339+Joseph2718@users.noreply.github.com>
2022-12-01 22:35:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 // Potential APIs: https://developer.chrome.com/docs/extensions/reference/
 // Tasks: get relevant extension APIs (e.g. chrome.alarms, chrome.notifications), get website title from URL and update table in index.html accordingly
+// Make the API requests here!
