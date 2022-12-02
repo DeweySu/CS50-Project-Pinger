63aaf5488b46287cb8b7449cfb43e7af96834f3e
DeweySu <112769574+DeweySu@users.noreply.github.com>
2022-12-02 04:17:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # For Flask (important: make sure all this stuff can be run by anyone!)
 import os
 
-# from cs50 import SQL
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -10,3 +10,4 @@ import re
 from datetime import datetime
 
 app = Flask(__name__)
+db = SQL("sqlite:///websites_monitored.db")
