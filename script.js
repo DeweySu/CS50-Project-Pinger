b0f18fb9ca4215eac18c66d0e053bf4085dff8b3
Joseph2718 <67449339+Joseph2718@users.noreply.github.com>
2022-12-01 23:29:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Potential APIs: https://developer.chrome.com/docs/extensions/reference/
 // Tasks: get relevant extension APIs (e.g. chrome.alarms, chrome.notifications), get website title from URL and update table in index.html accordingly
 // Make the API requests here!
+chrome.notifications.create('ping', {
+    type: 'basic',
+    iconUrl: 'pinger.png',
+    title: 'Update!',
+    message: 'A website you are tracking has been updated!',
+    priority: 2
+});
