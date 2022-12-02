57d07ebf4137ff7996982e3fcc88471e948af9fc
DeweySu <112769574+DeweySu@users.noreply.github.com>
2022-12-02 03:06:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+async function addWebsite() {
+    let url_to_add = document.getElementById("newURL").value;
+    const res=await fetch (url_to_add);
+    const res_text = res.text();
+
+    var tableRow = document.getElementById("websiteTable");
+    var row = document.createElement("tr");
+    var name = document.createElement("td");
+    var url = document.createElement("td");
+    var last_updated = document.createElement("td");
+    name.innerHTML = res_text.title;
+    cell2.innerHTML = url_to_add;
+    cell3.innerHTML = "N/A";
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    tableRow.appendChild(row);
+}
+addWebsite();
+/*
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#temp').addEventListener('click', function() {
         chrome.notifications.create('ping', {
@@ -9,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+*/
 /* Getting title from URL (probably not complete) | Source: https://stackoverflow.com/questions/60291497/is-there-a-way-to-get-a-websites-full-title-from-the-google-custom-search-api
 function httpGet(theUrl)
 {
