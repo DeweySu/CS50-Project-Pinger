0133ccac88f927ad6f3c20975eb066e9a3dc8d2a
Joseph2718 <67449339+Joseph2718@users.noreply.github.com>
2022-12-02 01:02:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const chrome = require('chrome');
-chrome.notifications.create('ping', {
-    type: 'basic',
-    iconUrl: 'pinger.png',
-    title: 'Update!',
-    message: 'A website you are tracking has been updated!',
-    priority: 2
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#submit').addEventListener('click', function() {
+        chrome.notifications.create('ping', {
+            type: 'basic',
+            iconUrl: 'pinger.png',
+            title: 'Update!',
+            message: 'A website you are tracking has been updated!',
+            priority: 2
+        });
+    });
 
 /* Getting title from URL (probably not complete) | Source: https://stackoverflow.com/questions/60291497/is-there-a-way-to-get-a-websites-full-title-from-the-google-custom-search-api
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
 
