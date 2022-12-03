df961608b32233fc680002d7407726e14df54b79
DeweySu <112769574+DeweySu@users.noreply.github.com>
2022-12-02 22:42:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,29 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
-
-import re
 from datetime import datetime
+import validators
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# A SQL database to store the websites we're monitoring
-# Columns are name, URL, times last updated, and the hash
+"""
+A SQL database to store the websites we're monitoring
+
+Schema:
+CREATE TABLE Websites (
+  name TEXT,
+  url TEXT,
+  last_updated datetime,
+  hash numeric
+);
+"""
 db = SQL("sqlite:///websites_monitored.db")
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -28,23 +38,35 @@ def after_request(response):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  # User reached route via GET (as by clicking a link or via redirect)
-
   # User reached route via POST (as by submitting a form via POST)
   if request.method == "POST":
-    url = request.form.get("username")
-    if not url:
+    url = request.form.get("newURL")
+
+    # If URL is invalid, redirect to main page and don't change anything
+    if not validators.url(url):
       return redirect("/")
-    # Next steps: find out how to get website name and how to hash the URL
-    db.execute("INSERT INTO Websites (url) VALUES (?)", url)
-    return redirect("/")
+
+    # Get the content of a webpage
+    webpage = requests.get(url)
+    
+    # Use BeautifulSoup to parse said webpage
+    soup = BeautifulSoup(webpage.text, 'html.parser')
+    
+    # Get title of webpage
+    title = soup.find('title')
+
+    # Hash contents of webpage for future comparison
+    current_hash = hash(webpage.text)
+
+    # Insert data into SQLite table
+    db.execute("INSERT INTO Websites (name, url, last_updated, hash) VALUES (?, ?, ?, ?);" title, url, datetime.now(), current_hash)
+
+  # User reached route via GET (as by clicking a link or via redirect)
   else:
-    websites_monitored = db.execute("SELECT DISTINCT * FROM Websites")
+    websites_monitored = db.execute("SELECT DISTINCT * FROM Websites;")
     return render_template("index.html", websites_monitored=websites_monitored)
 
 """
 if update...:
     flash("Ping! One of your websites has changed.")
 """
-# Use this regex to validate URL input: "^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
-    # Source: https://uibakery.io/regex-library/url-regex-python
